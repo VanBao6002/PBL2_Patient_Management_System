698b9bb8666db779e71863d54c90f58c8213c5b4
Văn Bảo <VanBao6002@gmail.com>
2025-08-19 21:12:35 +0700

    person0

---
+++ include/patient.h
@@ -0,0 +1,12 @@
+#ifndef PATIENT_H
+#define PATIENT_H
+
+#include "person.h"
+#include <string>
+class Patient : public Person{
+    private:
+    
+    public:
+};
+
+#endif
--- src/person.cpp
+++ src/person.cpp
@@ -1,4 +1,5 @@
 #include "person.h"
+#include <stdexcept>
 using namespace std;
 
 //construtor
@@ -12,13 +13,49 @@ void Person::setName(const string &name_){
 }
 
 void Person::setGender(char gender_){
-    gender = gender_;
+    if (std::tolower(gender_) != 'm' || std::tolower(gender_) != 'f'){
+        throw std::invalid_argument("Gioi tinh phai la 'M hoac 'F");
+    }
+    else {
+        gender = gender_;
+    }
 }
 
 void Person::setBirthday(const Date &birthday_){
+    if (birthday_.month < 1 || birthday_.month > 12){
+        throw std::invalid_argument("Thang sinh phai tu 1 - 12");
+    }
+
+    int maxDay;
+    switch(birthday_.month){
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12: 
+            maxDay = 31; break;
+        case 4: case 6: case 9: case 11: 
+            maxDay = 30; break;
+        case 2:
+            if ((birthday_.year % 4 == 0 && birthday_.year % 100 != 0) || (birthday_.year % 400 == 0))
+                maxDay = 29; 
+            else 
+                maxDay = 28;
+            break;
+    }
+
+    if (birthday_.day < 1 || birthday_.day > maxDay){
+        throw std::invalid_argument("Ngay sinh phai khong hop le");
+    }
+
+    if (birthday_.year < 1900 || birthday_.year > 2025){
+        throw std::invalid_argument("Nam sinh vuot qua pham vi cho phep");
+    }
+    
     birthday = birthday_;
 }
 
 void Person::setID(int ID_){
-    ID = ID_;
+    if (ID_ < 0){
+        throw std::invalid_argument("ID phai lon hon 0");
+    }
+    else{
+        ID = ID_;
+    }
 }
