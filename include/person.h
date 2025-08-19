40376f7b4f9fa2a7656f9b66808f29ffe2176495
Văn Bảo <VanBao6002@gmail.com>
2025-08-19 12:55:50 +0700

    person0

--- include/person.h
+++ include/person.h
@@ -1,9 +1,31 @@
-#ifndef PERSON_H
-#define PERSON_H
+    #ifndef PERSON_H
+    #define PERSON_H
 
-class Person {
-    
-    
-};
+    #include <string>
+    #include "date.h"
+    class Person {
+        protected:
+            std::string name;
+            char gender;
+            Date birthday;
+            int ID;
+        public:
+            //constructor
+            Person(const std::string &name, char gender, const Date &birthday, int ID);
+            virtual ~Person(); //virtual destructor 
 
-#endif
+            //pure virtual func
+            virtual void displayInfo() const = 0;
+
+            std::string getName() const {return name;}
+            char getGender() const {return gender;}
+            Date getBirthday() const {return birthday;}
+            int getID() const {return ID;}
+
+            void setName(const std::string &name_);
+            void setGender(const char gender_);
+            void setBirthday(const Date &birthday_); 
+            void setID(int ID_); 
+    };
+
+    #endif
