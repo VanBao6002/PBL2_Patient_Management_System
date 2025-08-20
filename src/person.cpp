e15ee8194c9f3d1864844c65899df3fdbca91446
Văn Bảo <VanBao6002@gmail.com>
2025-08-20 09:32:30 +0700

    Utils1

--- src/person.cpp
+++ src/person.cpp
@@ -3,12 +3,21 @@
 #include <cctype>
 #include <stdexcept>
 
-//construtor
-Person::Person(const std::string &name_, char gender_, const Date &birthday_, int ID_) : name(name_), gender(gender_), birthday(birthday_), ID(ID_) {}
+//default constructor
+Person::Person() : name(""), gender('M'), birthday(Date()), ID(0) {}
+
+//parameterized constructor
+Person::Person(const std::string &name_, char gender_, const Date &birthday_, int ID_) : name(name_), gender(gender_), birthday(birthday_), ID(ID_) {
+    Utils::validateName(name_);
+    Utils::validateGender(gender_);
+    Utils::validateBirthday(birthday_);
+    Utils::validateID(ID_);
+}
 
 //destructor
 Person::~Person() {}
 
+//setters
 void Person::setName(const std::string &name_){
     Utils::validateName(name_);
     name = name_;
